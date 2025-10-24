69f84a9d0f48c27178a0b1a07bec4ec2fa2314af
harold <haroldsultan@gmail.com>
2025-10-24 07:23:51 -0400

    Initial Commit

---
+++ Primedor/PrimedorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrimedorApp.swift
+//  Primedor
+//
+//  Created by harold sultan on 10/24/25.
+//
+
+import SwiftUI
+
+@main
+struct PrimedorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
